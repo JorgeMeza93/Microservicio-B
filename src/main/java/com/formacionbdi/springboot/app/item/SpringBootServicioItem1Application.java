86fb4a3a550490ca21a2d7ed4870d7df79492f62
Jorge Meza Bello <jorberiver@gmail.com>
2022-12-08 00:02:47 -0600

    servicio 2listo

---
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringBootServicioItem1Application.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServicioItem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServicioItem1Application.class, args);
+	}
+
+}
