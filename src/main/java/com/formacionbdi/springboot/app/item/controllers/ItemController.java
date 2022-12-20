de2ac3a65b04c0fae0061d7c27aafebe0f8cae37
Jorge Meza Bello <jorberiver@gmail.com>
2022-12-19 21:43:01 -0600

    Cambios extras

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,6 +1,7 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,7 @@ import com.formacionbdi.springboot.app.item.models.Producto;
 import com.formacionbdi.springboot.app.item.models.service.ItemService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 public class ItemController {
@@ -38,11 +40,19 @@ public class ItemController {
 		//return itemService.findById(id, cantidad);
 	}
 	
-	@CircuitBreaker(name="items")
+	@CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver2/{id}/cantidad/{cantidad}")
 	public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
 		
 			return itemService.findById(id, cantidad);
+	}
+	
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+	@TimeLimiter(name="items")
+	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
+	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
+		
+			return CompletableFuture.supplyAsync( () ->  itemService.findById(id, cantidad));
 	} 	
 	
 	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
@@ -55,6 +65,18 @@ public class ItemController {
 		producto.setPrecio(6000.00);
 		item.setProducto(producto);
 		return item;
+	}
+	
+	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
+		Item item = new Item();
+		Producto producto = new Producto();
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Consola Xbox");
+		producto.setPrecio(6000.00);
+		item.setProducto(producto);
+		return CompletableFuture.supplyAsync( () -> item);
 	}
 	
 }
